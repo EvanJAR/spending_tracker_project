cd7a814a8a7fc7a2f53848ab6a0ef272b46b8876
Evan Reid <evan_1995@hotmail.com>
2020-11-29 22:40:06 +0000

    tables created in the database, tags, merchants, transactions

--- db/spending_tracker.sql
+++ db/spending_tracker.sql
@@ -1,13 +1,20 @@
--- NEED TO REMEMBER TO DROP TABLES 
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS merchants;
 
 CREATE TABLE tags (
-
+    id  SERIAL PRIMARY KEY,
+    category VARCHAR(255)
 );
 
 CREATE TABLE merchants (
-
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
 );
 
 CREATE TABLE transactions (
-
+    id SERIAL PRIMARY KEY,
+    amount INT,
+    merchant_id INT REFERENCES merchants(id) ON DELETE CASCADE,
+    tag_id INT REFERENCES tags(id) ON DELETE CASCADE
 );
