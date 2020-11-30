a2687a80f7f25237437b348ef65d2e84f56e26da
Evan Reid <evan_1995@hotmail.com>
2020-11-30 16:51:05 +0000

    merchant and tag repositories got CRUD commands

---
+++ repositories/merchant_repository.py
@@ -0,0 +1,48 @@
+from db.run_sql import run_sql
+from models.merchant import Merchant
+from models.transaction import Transaction
+from models.tag import Tag
+
+
+#CREATE - new merchant
+def save(merchant):
+    sql = "INSERT INTO merchants (name) VALUES (%s) RETURNING id"
+    values = [merchant.name]
+    results = run_sql(sql, values)
+    merchant.id = results[0]['id']
+    return merchant
+
+#READ - select one by id
+def select(id):
+    merchant = None
+    sql = "SELECT * FROM merchants WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        merchant = Merchant(result['name'], result['id'])
+    return merchant 
+
+#READ - select all merchants
+def select_all():
+    merchants = []
+
+    sql = "SELECT * FROM merchants"
+    results = run_sql(sql)
+
+    for row in results:
+        merchant = Merchant(row['name'], row['id'])
+        merchants.append(merchant)
+    return merchants
+
+#UPDATE 
+#DELETE - all
+def delete_all():
+    sql = "DELETE FROM merchants"
+    run_sql(sql)
+
+#DELETE - one
+def delete(id):
+    sql = "DELETE FROM merchants WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
---
+++ repositories/tag_repository.py
@@ -0,0 +1,48 @@
+from db.run_sql import run_sql
+from models.merchant import Merchant
+from models.transaction import Transaction
+from models.tag import Tag
+
+
+#CREATE - new tag
+def save(tag):
+    sql = "INSERT INTO tags (category) VALUES (%s) RETURNING id"
+    values = [tag.name]
+    results = run_sql(sql, values)
+    tag.id = results[0]['id']
+    return tag
+
+#READ - select one by id
+def select(id):
+    tag = None
+    sql = "SELECT * FROM tags WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        tag = Tag(result['category'], result['id'])
+    return tag 
+
+#READ - select all merchants
+def select_all():
+    tags = []
+
+    sql = "SELECT * FROM tags"
+    results = run_sql(sql)
+
+    for row in results:
+        tag = Tag(row['category'], row['id'])
+        tag.append(tag)
+    return tags
+
+#UPDATE 
+#DELETE - all
+def delete_all():
+    sql = "DELETE FROM tags"
+    run_sql(sql)
+
+#DELETE - one
+def delete(id):
+    sql = "DELETE FROM tags WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
