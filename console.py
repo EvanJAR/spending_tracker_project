b7daa57a3d39e3def7cc8475f1ae0ba3dd22e236
Evan Reid <evan_1995@hotmail.com>
2020-12-01 11:06:10 +0000

    console data created

---
+++ console.py
@@ -0,0 +1,32 @@
+from models.tag import Tag
+from models.merchant import Merchant
+from models.transaction import Transaction
+
+import repositories.tag_repository as tag_repository
+import repositories.merchant_repository as merchant_repository
+import repositories.transaction_repository as transaction_repository
+
+transaction_repository.delete_all
+tag_repository.delete_all()
+merchant_repository.delete_all()
+
+tag1 = Tag("Entertainment")
+tag_repository.save(tag1)
+
+tag2 = Tag("Transport")
+tag_repository.save(tag2)
+
+tag3 = Tag("Food")
+
+merchant1 = Merchant("Amazon")
+merchant_repository.save(merchant1)
+
+merchant2 = Merchant("ScotRail")
+merchant_repository.save(merchant2)
+
+transaction1 = Transaction(merchant1, tag1, 25)
+transaction_repository.save(transaction1)
+
+transaction2 = Transaction(merchant2, tag2, 50)
+transaction_repository.save(transaction1)
+
