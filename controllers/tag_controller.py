2aef49eb3c17417a588698c19d482fb07134d79a
Evan Reid <evan_1995@hotmail.com>
2020-12-01 22:31:28 +0000

    edit tags now available

--- controllers/tag_controller.py
+++ controllers/tag_controller.py
@@ -15,3 +15,16 @@ def show(id):
     tag_merchants = tag_repository.merchants(tag)
     return render_template("tags/show.html", tag=tag, tag_merchants=tag_merchants)
 
+
+@tags_blueprint.route("/tags/<id>/edit")
+def edit_tag(id):
+    tag = tag_repository.select(id)
+    return render_template('tags/edit.html', tag=tag)
+
+
+@tags_blueprint.route("/tags/<id>", methods=["POST"])
+def update_tags(id):
+    category = request.form["category"]
+    tag = Tag(category, id)
+    tag_repository.update(tag)
+    return redirect("/tags")
