7730a66adb4d0f34e3bda8c7ee33d8d0d15749e2
Evan Reid <evan_1995@hotmail.com>
2020-12-01 10:54:34 +0000

    imported tag and merchant repositories to transaction repo

--- repositories/transaction_repository.py
+++ repositories/transaction_repository.py
@@ -1,7 +1,7 @@
 from db.run_sql import run_sql
-from models.merchant import Merchant
 from models.transaction import Transaction
-from models.tag import Tag
+import repositories.merchant_repository as merchant_repository
+import repositories.tag_repository as tag_repository
 
 def save(transaction):
     sql = "INSERT INTO transactions (merchant_id, tag_id, amount ) VALUES ( %s, %s, %s ) RETURNING id"
